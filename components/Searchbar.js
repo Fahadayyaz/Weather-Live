f31eb299b8a341618668eb8acf4ee1a11720ba2c
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-30 17:04:43 +0500

    Searchbar.js updated

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,12 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import React from "react";
+import Feather from "@expo/vector-icons/Feather";
 
 const Searchbar = () => {
   return (
-    <View>
-      <Text>Searchbar</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.contentContainer}>
+        <View style={{ flexDirection: "row" }}>
+          <TextInput />
+          <Feather name="search" size={24} color="black" />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default Searchbar;
+
+const styles = StyleSheet.create({
+  mainContainer: { flex: 1 },
+  contentContainer: {
+    width: "90%",
+    height: "100%",
+    backgroundColor: "lightblue",
+    flexDirection: "row",
+  },
+});
