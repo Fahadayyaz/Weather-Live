3fcc6a71de2ad0cff4e00113f35a7e5d7396a0d9
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-30 17:04:24 +0500

    App.js updated

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Searchbar from "./components/Searchbar";
+import Weather from "./components/Weather";
+const API_KEY = "0cb673ded64d558cc8e4652e007745b3";
+const API =
+  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
 export default function App() {
   return (
     <View>
-      <Text>App</Text>
+      <Searchbar />
+      <Weather />
     </View>
   );
 }
