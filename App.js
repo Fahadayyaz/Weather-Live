93f9dcd810e6b5205ee80676d21a277d921c3c7c
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-31 15:49:25 +0500

    App.js updated

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import Weather from "./components/Weather";
 
 const API_KEY = "0cb673ded64d558cc8e4652e007745b3";
 const API =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=" +
-  API_KEY;
+  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
 export default function App() {
   return (
@@ -20,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
     backgroundColor: "#fff",
+    alignItems: "center",
     justifyContent: "center",
   },
 });
