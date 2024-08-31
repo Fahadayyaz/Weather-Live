bb668c63e0b20f3df8017bcadf4186d5daf83f16
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-31 15:49:11 +0500

    Weather.js updated

--- components/Weather.js
+++ components/Weather.js
@@ -1,37 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
 
-const Weather = ({ city, temperature, condition }) => {
+const Weather = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.city}>{city}</Text>
-      <Text style={styles.temperature}>{temperature}°C</Text>
-      <Text style={styles.condition}>{condition}</Text>
+    <View>
+      <Text>Weather</Text>
     </View>
   );
 };
 
 export default Weather;
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    marginTop: 20,
-    padding: 20,
-    backgroundColor: "lightblue",
-    borderRadius: 10,
-  },
-  city: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  temperature: {
-    fontSize: 48,
-    fontWeight: "bold",
-    color: "#ff6347",
-  },
-  condition: {
-    fontSize: 20,
-    fontStyle: "italic",
-  },
-});
