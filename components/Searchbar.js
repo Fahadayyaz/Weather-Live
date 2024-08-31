e9aad5953208c4cfc579ee27ec2abacf2081e50a
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-31 15:48:59 +0500

    searchbar.js updated

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,14 +1,12 @@
-import { View, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet, Dimensions } from "react-native";
 import React from "react";
 import Feather from "@expo/vector-icons/Feather";
 
 const Searchbar = () => {
   return (
-    <View style={styles.contentContainer}>
-      <View style={styles.searchContainer}>
-        <TextInput style={styles.textInput} placeholder="Search city" />
-        <Feather name="search" size={24} color="black" />
-      </View>
+    <View style={styles.searchBar}>
+      <TextInput placeholder="Enter Your City Name" />
+      <Feather name="search" size={24} color="black" />
     </View>
   );
 };
@@ -16,24 +14,13 @@ const Searchbar = () => {
 export default Searchbar;
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    width: "100%",
-    padding: 10,
-    backgroundColor: "lightblue",
-    borderRadius: 10,
-    alignSelf: "center",
-    marginTop: 20,
-  },
-  searchContainer: {
+  searchBar: {
     flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "white",
+    justifyContent: "space-between",
+    borderWidth: 1.5,
+    borderColor: "black",
     borderRadius: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-  },
-  textInput: {
-    flex: 1,
-    marginRight: 10,
+    width: Dimensions.get("screen").width - 80,
+    padding: 10,
   },
 });
