d20560fb3f537b0ebd6df67cc0ec6974233c8ffb
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-09-02 12:10:17 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Searchbar from "./components/Searchbar";
 import Weather from "./components/Weather";
 
-const API_KEY = "0cb673ded64d558cc8e4652e007745b3";
-const API =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-
 export default function App() {
+  const [savedName, setSavedName] = useState("");
+  function cityNameHandler(cityName) {
+    setSavedName(cityName);
+  }
   return (
     <View style={styles.container}>
-      <Searchbar />
-      <Weather />
+      <Searchbar cityName={cityNameHandler} />
+      <Weather cityName={savedName} />
     </View>
   );
 }
